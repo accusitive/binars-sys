4d88ad57baed46824ddc561252dda9d5b9794c93
Rusty Shackleford <a@neb.best>
2020-11-03 02:00:29 -0500

    initial

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! # binars
+//! Binars system crate. dont use
+#![allow(dead_code)]
+#![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
---
+++ wrapper.h
@@ -0,0 +1,2 @@
+#include <binaryen-c.h>
+
