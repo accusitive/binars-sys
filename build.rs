432fdeb591ecd05f970c62a0bec4bcaeeefe1e7b
Rusty Shackleford <a@neb.best>
2020-11-04 06:31:45 -0500

    Reverting from automated build

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 extern crate bindgen;
 use std::env;
 use std::path::PathBuf;
-fn get_generator()->  &'static str {
+/* fn get_generator()->  &'static str {
     if cfg!(windows){
         return "Visual Studio 15 2017"
     }else if cfg!(mac){
@@ -11,18 +11,19 @@ fn get_generator()->  &'static str {
         "ninja"
     }
 }
+*/ 
 fn main() {
     // let p = cmake::build("binaryen");
-    let p = cmake::Config::new("binaryen").generator(get_generator()).build();
-    std::fs::write("testing123", format!("{:?}", p)).unwrap();
-    println!("{:?}", p);
-    println!("cargo:rustc-link-search={}/lib", p.display());
+    //let p = cmake::Config::new("binaryen").generator(get_generator()).build();
+    //std::fs::write("testing123", format!("{:?}", p)).unwrap();
+    //println!("{:?}", p);
+   // println!("cargo:rustc-link-search={}/lib", p.display());
 
     println!("cargo:rustc-link-lib=binaryen");
-    println!("cargo:rerun-if-changed={}", format!("{}/include/binaryen-c.h", p.to_str().unwrap()));
+   // println!("cargo:rerun-if-changed={}", format!("{}/include/binaryen-c.h", p.to_str().unwrap()));
     
     let bindings = bindgen::Builder::default()
-        .header(format!("{}/include/binaryen-c.h", p.display()))
+        .header("wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
